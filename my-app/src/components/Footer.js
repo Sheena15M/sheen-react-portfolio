5037a8b505d3c9ca775cac69a84494f0c6627682
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-29 15:33:39 -0500

    footer started

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class Footer extends Component {
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <footer>
+                <div className="row">
+                    <div className="twelve columns">
+                </div>
+            </footer>
+        )
+    }
+}
