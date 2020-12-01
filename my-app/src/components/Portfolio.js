33be9d4e261792e26117a76bf67b41eed666fdae
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-30 21:13:09 -0500

    portfolio added/started

---
+++ my-app/src/components/Portfolio.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>The foundational masterpieces</h1>
