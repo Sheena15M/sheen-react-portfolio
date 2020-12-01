554a2b340266f6bd1ff2d1b46f14cc404dbf1fc2
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-11-30 21:14:27 -0500

    portfolio finished, for now

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -7,3 +7,30 @@ export default class Porfolio extends Component {
       <div className="row">
         <div className="twelve columns collapsed">
           <h1>The foundational masterpieces</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01"></a>
+                    <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
